84e0203c9f1e5028a232cbb0dfb9a44b9ee80471
sebasvisser <sebas.visser@mensenrelatie.nl>
2024-03-22 21:30:43 +0100

    opzetten van beide arrays en pseudo code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 
 public class Main {
     public static void main(String[] args) {
+        Integer[] numeric = {0,1,2,3,4,5,6,7,8,9};
+        String[] alphabetic = {"nul","een","twee","drie","vier","vijf","zes","zeven","acht","negen"};
 
-        // Hier mag je je code schrijven voor de hoofd-opdracht
-        //
-
+//        e gaat een applicatie bouwen die getallen vertaalt van numeriek (1, 2, 3, etc.) naar alfabetisch (een, twee, drie, etc.).
+//        Je gaat die getallen vertalen door gebruik te maken van een HashMap.
+//        Je zet de numerieke getallen als sleutel (key) en de alfabetische getallen als waarde (value) in de HashMap.
+//        Vervolgens vraag je de gebruiker om een input van 0 t/m 9 te geven en ga je dat "vertalen" door simpelweg de waarde uit de HashMap te vragen met de bijbehorende sleutel
+//        en dat terug te geven aan de gebruiker.
+//        - In je main methode staan twee arrays (1 numeriek en 1 alfabetisch), een boolean variabele, een Translator object en een Scanner object;
+//        - Je project bevat 1 Translator class met daarin een HashMap variabele, een constructor met 2 arrays als parameter en een translate functie;
+//        - De logica van de applicatie wordt gedraaid in een while(boolean)-loop in je main methode.
 
     }
 }
---
+++ src/Translator.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class Translator {
+    HashMap<Integer,String> numericAlpha =
+}
+
+//
+//Maak in de Translator class een `HashMap<Integer,String>` variabele met de naam `numericAlpha`.
+//        6. Maak in de Translator class een constructor die de volgende parameters krijgt: `(String[] alphabetic, Integer[] numeric)`.
+//        7. Schrijf in de constructor een for-loop die begint bij 0 en doorgaat tot de lengte van de numeric/alphabetic array (maakt niet uit welke, ze zijn even lang).
+//        8. Voeg in de body van de for-loop een nieuwe entry toe aan de HashMap met de correcte waardes uit `numeric` en `alphabetic`. Gebruik de `i` variabele uit je for-loop om de correcte waardes uit de arrays te halen.
