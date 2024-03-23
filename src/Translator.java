67345bd5506624914085634dfb433a87cb6b7055
sebasvisser <sebas.visser@mensenrelatie.nl>
2024-03-23 13:33:43 +0100

    stap 6, 7 en 8 translator class aangemaakt met constructor en logica

--- src/Translator.java
+++ src/Translator.java
@@ -1,11 +1,19 @@
 import java.util.HashMap;
 
 public class Translator {
-    HashMap<Integer,String> numericAlpha =
+    HashMap<Integer, String> numericAlpha = new HashMap<Integer, String>();
+
+    public Translator(String[] alphabetic, Integer[] numeric) {
+        // Check if both arrays have the same length to avoid IndexOutOfBoundsException
+        if (alphabetic.length != numeric.length) {
+            throw new IllegalArgumentException("alphabetic and numeric arrays should be of equal length.");
+        }
+
+        // Loop through one of the arrays length
+        for (int i = 0; i < alphabetic.length; i++) {
+            // Populate the HashMap with elements from both arrays
+            numericAlpha.put(numeric[i], alphabetic[i]);
+        }
+    }
 }
 
-//
-//Maak in de Translator class een `HashMap<Integer,String>` variabele met de naam `numericAlpha`.
-//        6. Maak in de Translator class een constructor die de volgende parameters krijgt: `(String[] alphabetic, Integer[] numeric)`.
-//        7. Schrijf in de constructor een for-loop die begint bij 0 en doorgaat tot de lengte van de numeric/alphabetic array (maakt niet uit welke, ze zijn even lang).
-//        8. Voeg in de body van de for-loop een nieuwe entry toe aan de HashMap met de correcte waardes uit `numeric` en `alphabetic`. Gebruik de `i` variabele uit je for-loop om de correcte waardes uit de arrays te halen.
