15eae27b97445b35dec7ac11dab007d94dbd30ce
sebasvisser <sebas.visser@mensenrelatie.nl>
2024-03-23 17:45:14 +0100

    opruimen en gele knringeltjes

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 
-
 public class Main {
     static Translator translator;
     public static void main(String[] args) {
         Integer[] numeric = {0,1,2,3,4,5,6,7,8,9};
         String[] alphabetic = {"nul","een","twee","drie","vier","vijf","zes","zeven","acht","negen"};
-        Boolean play = true;
+        boolean play = true;
         String ongeldig = "Ongeldige Invoer";
         Scanner scanner = new Scanner(System.in);
         translator = new Translator(alphabetic,numeric);
-
 
         while(play) {
             System.out.println("Type 'x' om te stoppen \n Type 'v' om te vertalen");
@@ -27,15 +25,11 @@ public class Main {
                     String result = translator.translate(numberInput);
                     System.out.println("De vertaling van " + numberInput + " is " + result);
                 } else {
-                    System.out.println("ongeldig");
+                    System.out.println(ongeldig);
                 }
             } else {
-                System.out.println("ongeldig");
+                System.out.println(ongeldig);
             }
         }
-
-
     }
-
-
 }
